0f1f292bdd9e976ed298cc3856e680ba0e51d2e7
Eniola Ajayi <ajayieniola16@gmail.com>
2026-03-23 14:50:06 +0000

    setup base ui for editor

--- text-editor/src/main.rs
+++ text-editor/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use iced::Element;
+use iced::widget::text;
+
+fn main() -> iced::Result {
+    iced::run(Editor::update, Editor::view)
+}
+
+#[derive(Default)]
+struct Editor;
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+impl Editor {
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        String::from("Text Editor")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {}
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+        text("Hello, iced!").into()
+    }
 }
