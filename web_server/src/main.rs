cc275240cb8d333612bc50ddc77c176a2750b60a
Eniola Ajayi <ajayieniola16@gmail.com>
2026-03-25 10:01:49 +0000

    setup thread pool

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -3,17 +3,23 @@ use std::{
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
     thread,
+    time::Duration,
 };
+use web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
-        });
+        })
+        // thread::spawn(|| {
+        //     handle_connection(stream);
+        // });
     }
 }
 
@@ -21,10 +27,13 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
